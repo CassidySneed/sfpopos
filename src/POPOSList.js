b76fea999bfd7e82b84795e3293f68b66e091b53
CassidySneed <teamsneedman@gmail.com>
2021-11-02 21:37:37 -0700

    content componet built

--- src/POPOSList.js
+++ src/POPOSList.js
@@ -5,12 +5,12 @@ import './POPOSList.css'
 function POPOSList() {
     return (
         <div className="POPOSList">
-            <POPOSSpace name="50 California Street" address="50 California St." image="50-california-st.jpg"/>
-            <POPOSSpace name="50 California Street" address="50 California St." image="50-california-st.jpg"/>
-            <POPOSSpace name="50 California Street" address="50 California St." image="50-california-st.jpg"/>
-            <POPOSSpace name="50 California Street" address="50 California St." image="50-california-st.jpg"/>
-            <POPOSSpace name="50 California Street" address="50 California St." image="50-california-st.jpg"/>
-            <POPOSSpace name="50 California Street" address="50 California St." image="50-california-st.jpg"/>
+            <POPOSSpace name="50 California Street" address="50 California St." image="sf.jpeg"/>
+            <POPOSSpace name="50 California Street" address="50 California St." image="sf.jpeg"/>
+            <POPOSSpace name="50 California Street" address="50 California St." image="sf.jpeg"/>
+            <POPOSSpace name="50 California Street" address="50 California St." image="sf.jpeg"/>
+            <POPOSSpace name="50 California Street" address="50 California St." image="sf.jpeg"/>
+            <POPOSSpace name="50 California Street" address="50 California St." image="sf.jpeg"/>
         </div>
     )
 }
--- src/POPOSSpace.js
+++ src/POPOSSpace.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './POPOSList.css'
 
 function POPOSSpace(props) {
+    const {name, image, address} = props
     return (
         <div className="POPOSSpace">
-            <img src={`${process.env.PUBLIC_URL}/images/sf.jpeg`} width="300" height="300" alt="POPOS" />
-            <h1>{props.name}</h1>
-            <p>{props.address}</p>
+            <img src={`${process.env.PUBLIC_URL}images/${image}`} width="300" height="300" alt="POPOS" />
+            <h1>{name}</h1>
+            <p>{address}</p>
         </div>
     )
 }
