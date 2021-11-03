14959218440e28877f61b1e7f7d540b8b35f58b4
CassidySneed <teamsneedman@gmail.com>
2021-11-02 21:54:17 -0700

    change

--- src/POPOSList.js
+++ src/POPOSList.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import POPOSSpace from './POPOSSpace';
 import './POPOSList.css'
+import data from './locations.json'
 
 function POPOSList() {
+    const spaces = data.map(({name, address, images, hours}) => {
+        return (
+            <POPOSSpace
+                name={name}
+                address={address}
+                image={images[0]}
+                hours={hours}
+            />
+        )
+    })
     return (
         <div className="POPOSList">
-            <POPOSSpace name="50 California Street" address="50 California St." image="sf.jpeg"/>
-            <POPOSSpace name="50 California Street" address="50 California St." image="sf.jpeg"/>
-            <POPOSSpace name="50 California Street" address="50 California St." image="sf.jpeg"/>
-            <POPOSSpace name="50 California Street" address="50 California St." image="sf.jpeg"/>
-            <POPOSSpace name="50 California Street" address="50 California St." image="sf.jpeg"/>
-            <POPOSSpace name="50 California Street" address="50 California St." image="sf.jpeg"/>
+            {spaces}
         </div>
     )
 }
--- src/POPOSSpace.js
+++ src/POPOSSpace.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './POPOSList.css'
 
 function POPOSSpace(props) {
-    const {name, image, address} = props
+    const {name, image, address, hours} = props
     return (
         <div className="POPOSSpace">
             <img src={`${process.env.PUBLIC_URL}images/${image}`} width="300" height="300" alt="POPOS" />
             <h1>{name}</h1>
             <p>{address}</p>
+            <p>{hours}</p>
         </div>
     )
 }
